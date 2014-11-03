10c0c5a054cf56acc3b48ce3397f28a2a46e57db
Dave Green <dave.green@moneysupermarket.com>
2014-11-03 20:32:38 +0000

    Initial Commit

---
+++ LoginSample/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  LoginSample
+//
+//  Created by Dave Green on 16/10/2014.
+//  Copyright (c) 2014 DeveloperDave. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  @IBOutlet weak var loginTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var loginButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 SwiftLoginSample
 ================
+
+For more info see the accompanying blog post: http://www.developerdave.co.uk/2014/10/login-pattern-part-1/
